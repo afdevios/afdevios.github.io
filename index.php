27836c5f0a5d6af30e676c5c92d6860ae4ffdcb6
kevin wang <fivepig@yeah.net>
2015-04-11 09:47:07 +0800

    no message

---
+++ index.php
@@ -0,0 +1,44 @@
+<html>
+    
+    <head>
+    	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+        <title>测试版App下载页</title>
+<meta name="viewport" content="width=device-width,height=device-height, user-scalable=no,initial-scale=1, minimum-scale=1, maximum-scale=1,target-densitydpi=device-dpi ">
+    
+    </head>
+    
+    <body>
+        <p></p>
+        <p></p>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+        
+        <a href ="itms-services://?action=download-manifest&url=https://afdev.sinaapp.com/af.plist">
+        	安装iphone测试版
+</a>
+
+<br/>
+<br/>
+	
+	<p></p>
+
+        <a href ="https://afdev.sinaapp.com/acfun_video.apk">
+        	安装android测试版
+</a>
+			
+			<br/>
+<br/>
+
+
+	<p></p>
+<a href ="http://ios8.sinaapp.com/udid">
+      查看你的iphone的UUID
+</a>
+
+    </body>
+    
+</html>
